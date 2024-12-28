7da898b6ba61121c4b0280f904bfe4ed7c612431
Alberto Paradís Llop <albertoparadisllop@gmail.com>
2024-12-28 21:50:01 +0100

    Fix startup with no token

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def main(token):
 
 if __name__ == '__main__':
     bot_token = ""
-    if len(sys.argv)==2 and "TOKEN" in os.environ:
+    if len(sys.argv)==2:
         # Try to get it from env
         print("Token from command line")
         bot_token = sys.argv[1]
-    else:
+    elif "TOKEN" in os.environ:
         print("Token from env var")
         bot_token = os.getenv("TOKEN")
+    else:
+        print("No bot token provided")
+        raise Exception("")
     main(bot_token)
